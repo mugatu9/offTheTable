dccae8ce25f6b79464cb748417890141fe36a02b
mcclure15 <m001010111@gmail.com>
2015-01-14 14:57:17 -0800

    add JS person constructor worked on

---
+++ foodArrays.js
@@ -0,0 +1,2 @@
+var foodCategories = []
+		fruit = []
--- offTheTable.js
+++ offTheTable.js
@@ -1,4 +1,6 @@
 <script>
+/*
+
 var foodCategories = [],
     fruit = ["banana", "orange", "kiwi", "apple", "mango", "cherry"],
     veggies = ["celery", "broccoli", "turnips", "cabbage", "carrots", "kale"],
@@ -9,27 +11,44 @@ var foodCategories = [],
 
 foodCategories.push(fruit, veggies, dairy, grains, nuts, legumes);
 
+*/
 
-function PeopleGroup() {
-  this.persons = [];
+function Person(name, allergies, aversions) {
+  this.name = name;
+  this.allergies = allergies;
+  this.aversions = aversions;
 }
-
-var groupPeeps = new PeopleGroup();
 
 PeopleGroup.prototype.addPerson = function(){
   this.persons.push(new Person(name, allergies, aversions));
 }
 
 
+// var nathan = new Person(["nuts", "dairy", "celery"], ["shrimp", "mushrooms"]);
+// var matthew = new Person(["gluten", "celery"], ["mayo", "shrimp", "sour cream"]);
 
-function Person(name) {
-  this.name = name;
-  this.aversions = [];
-  this.allergies = [];
-}
+
+groupPeeps.addPerson("Nathan", ["nuts", "dairy", "celery"], ["shrimp", "mushrooms"]);
+groupPeeps.addPerson("Matthew", ["gluten", "celery"], ["mayo", "shrimp", "sour cream"]);
+
+
+
 Person.aversions = function() {
 
 }
+
+function PeopleGroup() {
+  this.persons = [];
+}
+
+
+var groupPeeps = new PeopleGroup();
+
+
+
+
+
+
 
 
 </script>
