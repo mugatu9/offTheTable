12f52588d9a27695c9a1c9a1fb65184ce4e44491
Nathan O'Brien <mugatu9@gmail.com>
2015-01-16 15:44:07 -0800

    make addPerson function compatible with html input and add event listener

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 <script>
-/*
-
-var foodCategories = [],
-    fruit = ["banana", "orange", "kiwi", "apple", "mango", "cherry"],
-    veggies = ["celery", "broccoli", "turnips", "cabbage", "carrots", "kale"],
-    dairy = ["milk", "cheese", "ice cream", "yogurt", "cream", "butter"],
-    grains = ["wheat", "oats", "barley", "rice", "rye", "amaranth"],
-    nuts = ["cashews", "almonds", "walnuts", "pecans", "chestnuts", "hazlenuts"],
-    legumes = ["peanuts", "beans", "soybeans", "lentils", "chickpeas", "alfalfa"];
-
-foodCategories.push(fruit, veggies, dairy, grains, nuts, legumes);
-
-*/
+var addPerson = getElementsByClassName("addPerson")[0];
+addPerson.addEventListenter('click',peopleGroup.addPerson, false);
 
 var masterAllergiesList = [],
-    masterAversionsList = [];
+    masterAversionsList = [],
+    counter = 1;
 
 function Person(name, allergies, aversions) {
   this.name = name;
@@ -26,21 +16,31 @@ function PeopleGroup() {
   this.persons = [];
 }
 
-PeopleGroup.prototype.addPerson = function(){
+PeopleGroup.prototype.addPerson = function(name, allergies, aversions){
+  var getInputs = document.getElementsByClassName("inputs").value);
+  var seperator = ",";
+  var name      = getInputs[0],
+      allergies = getInputs[1].split(seperator),
+      aversions = getInputs[2].split(seperator);
   this.persons.push(new Person(name, allergies, aversions));
+
+  counter++;
 }
 
 var peopleGroup = new PeopleGroup();
 
-peopleGroup.addPerson("Nathan", ["nuts", "dairy", "celery"], ["shrimp", "mushrooms"]);
-peopleGroup.addPerson("Matthew", ["gluten", "celery"], ["mayo", "shrimp", "sour cream"]);
+peopleGroup.addPerson("Nathan",  ["nuts", "dairy", "celery"], ["shrimp", "mushrooms"]);
+peopleGroup.addPerson("Matthew", ["gluten", "celery"],        ["mayo", "shrimp", "sour cream"]);
 
 for(i=0; i<peopleGroup.persons.length; i++){
   masterAllergiesList.push(peopleGroup.persons[i].allergies);
   masterAversionsList.push(peopleGroup.persons[i].aversions);
 }
 
+
+function generateOutput () {}
 console.log("The group's allergies are " + masterAllergiesList);
 console.log("The group's aversions are " + masterAversionsList);
+
 
 </script>
